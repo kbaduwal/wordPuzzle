f8df64bc4594aa05d3cacd74bbf0c3c02e1a07ce
kbaduwal <khakendra1999@gmail.com>
2023-09-07 22:54:20 +0530

    The logic of the word game is completed

--- src/io/mkbaduwa/Grid.java
+++ src/io/mkbaduwa/Grid.java
@@ -1,13 +1,25 @@
 package io.mkbaduwa;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Grid {
     private int gridSize ;
     private char[][] contents;
     private List<Coordinate> coordinates = new ArrayList<>();
+
+    private enum Direction{
+        HORIZONTAL,
+        VERTICAL,
+        DIAGONAL,
+        HORIZONTAL_INVERSE,
+        VERTICAL_INVERSE,
+        DIAGONAL_INVERSE
+
+    }
 
     private class Coordinate {
         int x;
@@ -32,20 +44,52 @@ public class Grid {
     }
 
     public void fillGrid(List<String> words){
+        Collections.shuffle(coordinates);
         for (String word : words) {
-            Collections.shuffle(coordinates);
-
             for (Coordinate coordinate : coordinates) {
                 int x = coordinate.x;
                 int y = coordinate.y;
-                if (doesFit(word,coordinate)) {
-                    for (char c : word.toCharArray()) {
-                        contents[x][y++] = c;
+                Direction selectedDirection = getDirectionForFit(word,coordinate);
+                if (selectedDirection != null) {
+                    switch (selectedDirection){
+                        case HORIZONTAL:
+                            for (char c : word.toCharArray()) {
+                                contents[x][y++] = c;
+                            }
+                            break;
+                        case VERTICAL:
+                            for (char c : word.toCharArray()) {
+                                contents[x++][y] = c;
+                            }
+                            break;
+                        case DIAGONAL:
+                            for (char c : word.toCharArray()) {
+                                contents[x++][y++] = c;
+                            }
+                            break;
+                        case HORIZONTAL_INVERSE:
+                            for (char c : word.toCharArray()) {
+                                contents[x][y--] = c;
+                            }
+                            break;
+                        case VERTICAL_INVERSE:
+                            for (char c : word.toCharArray()) {
+                                contents[x--][y] = c;
+                            }
+                            break;
+                        case DIAGONAL_INVERSE:
+                            for (char c : word.toCharArray()) {
+                                contents[x--][y--] = c;
+                            }
+                            break;
+
                     }
                     break;
+
                 }
             }
         }
+        randomFillGrid();
     }
 
     public void displayGrid(){
@@ -57,14 +101,85 @@ public class Grid {
         }
     }
 
-    private boolean doesFit(String word, Coordinate coordinate){
-        if(coordinate.y + word.length() < gridSize){
-            for(int i=0; i<word.length(); i++){
-               if( contents[coordinate.x][coordinate.y + i] != '_') return false;
+    private void randomFillGrid(){
+        String allCapLetters ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        for (int i=0; i<gridSize; i++){
+            for(int j=0; j<gridSize; j++) {
+                if(contents[i][j]=='_'){
+                    int randomIndex = ThreadLocalRandom.current().nextInt(0, allCapLetters.length());
+                    contents[i][j] = allCapLetters.charAt(randomIndex);
+
+                }
             }
-            return true;
         }
-        return false;
+    }
+
+    private Direction getDirectionForFit(String word, Coordinate coordinate){
+        List<Direction> directions = Arrays.asList(Direction.values());
+        Collections.shuffle(directions);
+        for (Direction direction: directions){
+            if(doesFit(word,coordinate,direction)){
+                return direction;
+            }
+
+        }
+        return null;
+    }
+
+    private boolean doesFit(String word, Coordinate coordinate, Direction direction){
+        int wordLength = word.length();
+        switch (direction){
+            case HORIZONTAL :
+                if(coordinate.y + wordLength >gridSize) return false;
+                for (int i=0; i< wordLength; i++){
+                    if(contents[coordinate.x][coordinate.y+i]!='_'){
+                        return false;
+                    }
+                }
+                break;
+            case VERTICAL:
+                if(coordinate.x + wordLength >gridSize) return false;
+                for (int i=0; i< wordLength; i++){
+                    if(contents[coordinate.x+i][coordinate.y]!='_'){
+                        return false;
+                    }
+                }
+                break;
+            case DIAGONAL:
+                if(coordinate.y + wordLength >gridSize || coordinate.x + wordLength >gridSize) return false;
+                for (int i=0; i< wordLength; i++){
+                    if(contents[coordinate.x+i][coordinate.y+i]!='_'){
+                        return false;
+                    }
+                }
+                break;
+            case HORIZONTAL_INVERSE:
+                if(coordinate.y < wordLength) return false;
+                for (int i=0; i< wordLength; i++){
+                    if(contents[coordinate.x][coordinate.y-i]!='_'){
+                        return false;
+                    }
+                }
+                break;
+            case VERTICAL_INVERSE:
+                if(coordinate.x < wordLength) return false;
+                for (int i=0; i< wordLength; i++){
+                    if(contents[coordinate.x-i][coordinate.y]!='_'){
+                        return false;
+                    }
+                }
+                break;
+            case DIAGONAL_INVERSE:
+                if(coordinate.y  <wordLength || coordinate.x < wordLength) return false;
+                for (int i=0; i< wordLength; i++){
+                    if(contents[coordinate.x-i][coordinate.y-i]!='_'){
+                        return false;
+                    }
+                }
+                break;
+
+        }
+        return true;
     }
 
 }
